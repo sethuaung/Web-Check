fd1d8818a11af9e49ba06a28cc2db7ce8e111cea
sethuaung <sethuaung@outlook.com>
2026-09-12 21:45:51 +0630

    updated route

--- src/client/components/misc/Footer.tsx
+++ src/client/components/misc/Footer.tsx
@@ -41,13 +41,13 @@ const ALink = styled.a`
 `;
 
 const Footer = (props: { isFixed?: boolean }): JSX.Element => {
-  const licenseUrl = 'https://github.com/lissy93/web-check/blob/master/LICENSE';
-  const authorUrl = 'https://aliciasykes.com';
-  const githubUrl = 'https://github.com/lissy93/web-check';
+  const licenseUrl = 'https://github.com/sethuaung/web-check/blob/master/LICENSE';
+  const authorUrl = 'https://felixent.net';
+  const githubUrl = 'https://github.com/sethuaung/web-check';
   return (
     <StyledFooter style={props.isFixed ? { position: 'fixed' } : {}}>
       <span>
-        View source at <ALink href={githubUrl}>github.com/lissy93/web-check</ALink>
+        View source at <ALink href={githubUrl}>github.com/sethuaung/web-check</ALink>
       </span>
       <span>
         <Link to="/about">Web-Check</Link> is licensed under <ALink href={licenseUrl}>MIT</ALink> -
